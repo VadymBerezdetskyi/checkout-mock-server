0152d0a10e10aeae9e279315f6b36cd6af490bf2
Vadym Berezdetskyi <berezdetskyi.v@paymaxi.com>
2020-03-19 18:09:05 +0200

    Mocks updated

--- functions/src/models/PaymentInvoice/data/commerceAccountMock.ts
+++ functions/src/models/PaymentInvoice/data/commerceAccountMock.ts
@@ -5,63 +5,83 @@ export const commerceAccountMock = {
   website: 'https://google.com',
   services: [
     {
-      code: 'card',
-      method: 'card',
+      code: 'googlepay',
+      flow: 'google_pay',
+      method: 'googlepay',
       currency: 'UAH',
+      fields: [],
+      context_fields: [],
+      total_amount: 105.5,
+      amount_min: 0.01,
+      amount_max: 100000,
+    },
+    {
+      code: 'applepay',
+      flow: 'apple_pay',
+      method: 'applepay',
+      currency: 'UAH',
+      fields: [],
+      context_fields: [],
+      total_amount: 105.5,
+      amount_min: 0.01,
+      amount_max: 100000,
+    },
+    {
+      code: 'card',
+      method: 'bank_card',
+      currency: 'XXX',
+      total_amount: 105.5,
+      amount_min: 0.01,
+      amount_max: 100000,
+      flow: 'card',
+      fields: [],
+      context_fields: [],
+    },
+    {
+      // service for accounts with type 'card'
+      code: 'checkout_card_token',
+      method: 'bank_card',
+      currency: 'XXX',
       total_amount: 105.5,
       amount_min: 0.01,
       amount_max: 100000,
       flow: 'card',
       fields: [
         {
-          key: 'email',
-          type: 'email',
-          required: false,
+          key: 'checkout_token',
+          type: 'text',
+          required: true,
           label: {
-            en: 'email',
+            en: '',
           },
           hint: {
-            en: ' hint',
-          },
-          regexp: '',
-          position: 0,
-        },
-      ],
-      context_fields: [
-        {
-          key: 'customer.phone',
-          type: 'string',
-          required: false,
-          label: {
-            en: 'Phone',
-          },
-          hint: {
-            en: 'Phone hint',
+            en: '',
           },
           regexp: '',
           position: 0,
         },
         {
-          key: 'billing_address.country',
-          type: 'string',
-          required: false,
+          key: 'cvv',
+          type: 'text',
+          required: true,
           label: {
-            en: 'line1',
+            en: '',
           },
           hint: {
-            en: 'line1 hint',
+            en: '',
           },
           regexp: '',
           position: 0,
         },
       ],
+      context_fields: [],
     },
     {
       code: 'alipay_uah',
       method: 'alipay',
       currency: 'UAH',
-      total_amount: 105.5,
       amount_min: 0.01,
+      total_amount: 105.5,
       amount_max: 100000,
       flow: 'hpp',
       fields: [
@@ -448,6 +468,62 @@ export const commerceAccountMock = {
     },
   ],
   methods: [
+    {
+      code: 'googlepay',
+      name: {
+        en: 'Google Pay',
+        ru: 'Google Pay',
+        uk: 'Google Pay',
+      },
+      description: {
+        en: 'Google Pay is a digital wallet and payment platform from Google.',
+      },
+      category: 'digital_wallet',
+      logo:
+        'https://static.openfintech.io/payment_methods/googlepay/logo.svg?w=400&c=v0.59.26#w200',
+      icon:
+        'https://static.openfintech.io/payment_methods/googlepay/icon.svg?w=278&c=v0.59.26#w100',
+      position: 0,
+      metadata: [],
+      hide: false,
+    },
+    {
+      code: 'applepay',
+      name: {
+        en: 'Apple Pay',
+        ru: 'Apple Pay',
+        uk: 'Apple Pay',
+      },
+      description: {
+        en:
+          'Apple Pay is the new mobile payment system by Apple. Apple Pay uses NFC technology for contactless payments in store, and can be used for online and in-app purchases.\u00a0',
+      },
+      category: 'digital_wallet',
+      logo: 'https://static.openfintech.io/payment_methods/applepay/logo.svg?w=400&c=v0.59.26#w200',
+      icon: 'https://static.openfintech.io/payment_methods/applepay/icon.svg?w=278&c=v0.59.26#w100',
+      position: 0,
+      metadata: [],
+      hide: false,
+    },
+    {
+      code: 'bank_card',
+      category: 'payment_card',
+      name: {
+        en: 'Card',
+        ru: 'Card',
+        uk: 'Card',
+      },
+      description: {
+        en: 'Bank card',
+      },
+      metadata: {},
+      position: 0,
+      hide: false,
+      logo:
+        'https://static.openfintech.io/payment_methods/bank_card/logo.svg?w=400&c=v0.59.26#w200',
+      icon:
+        'https://static.openfintech.io/payment_methods/bank_card/icon.svg?w=278&c=v0.59.26#w100',
+    },
     {
       code: 'alipay',
       category: 'digital_wallet',
